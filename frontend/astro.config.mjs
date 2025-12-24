1f5fa310b4eb0f72dc3f502e4f68aa2ba3b89dec
Maxknuff <derknuffelbuffel@gmail.com>
2025-12-24 23:21:57 +0000

    feat: add Vercel configuration and update Astro config for server output

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless"; // Der neue Adapter
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // WICHTIG: Schaltet auf Live-Server um
+  output: 'server',
   adapter: vercel(),
   integrations: [tailwind(), react()]
 });
