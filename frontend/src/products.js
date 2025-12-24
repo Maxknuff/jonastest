7a29f0e8bf980beda2a0a3b7598ab4734fe80d5c
Maxknuff <derknuffelbuffel@gmail.com>
2025-12-24 16:29:12 +0000

    Fix comment typo in ProductSchema to correct German translation

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const ProductSchema = new mongoose.Schema({
   productId: { type: String, required: true, unique: true },
-  // DIESE NEUEN FELDER FEHLEN NOCH BEI DIR:
+  // Diese Felder haben gefehlt:
   name: { type: String, required: true },
   price: { type: Number, required: true },
   image: { type: String, required: true },
